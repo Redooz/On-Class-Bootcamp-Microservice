f86ac46c46ebcc2d35567c327b747bdae99226e3
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-12 09:34:37 -0500

    feat: add interface repository for Technology

---
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/repository/ITechnologyRepository.java
@@ -0,0 +1,6 @@
+package com.redoz.onclass.adapters.driven.jpa.mysql.repository;
+
+import com.redoz.onclass.adapters.driven.jpa.mysql.entity.TechnologyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITechnologyRepository extends JpaRepository<TechnologyEntity, Long>{}
