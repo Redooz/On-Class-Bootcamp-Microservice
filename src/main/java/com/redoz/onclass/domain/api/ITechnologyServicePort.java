2c0e775975d69fdb14462815c22849720f1a23f1
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-11 23:26:42 -0500

    feat: add interface for technology service port

---
+++ src/main/java/com/redoz/onclass/domain/api/ITechnologyServicePort.java
@@ -0,0 +1,7 @@
+package com.redoz.onclass.domain.api;
+
+import com.redoz.onclass.domain.model.Technology;
+
+public interface ITechnologyServicePort {
+    void saveTechnology(Technology technology);
+}
