f3a1dbd34be2e329e550fc503d160801147f411b
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-12 09:45:46 -0500

    feat: add implementation of ITechnologyPersistencePort, TechnologyAdapter

---
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/adapter/TechnologyAdapter.java
@@ -0,0 +1,22 @@
+package com.redoz.onclass.adapters.driven.jpa.mysql.adapter;
+
+import com.redoz.onclass.adapters.driven.jpa.mysql.exception.TechnologyAlreadyExistsException;
+import com.redoz.onclass.adapters.driven.jpa.mysql.mapper.ITechnologyEntityMapper;
+import com.redoz.onclass.adapters.driven.jpa.mysql.repository.ITechnologyRepository;
+import com.redoz.onclass.domain.model.Technology;
+import com.redoz.onclass.domain.spi.ITechnologyPersistencePort;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class TechnologyAdapter implements ITechnologyPersistencePort {
+    private final ITechnologyRepository technologyRepository;
+    private final ITechnologyEntityMapper technologyEntityMapper;
+
+    @Override
+    public void saveTechnology(Technology technology) {
+        if (technologyRepository.existsById(technology.getId())) {
+            throw new TechnologyAlreadyExistsException();
+        }
+        technologyRepository.save(technologyEntityMapper.toEntity(technology));
+    }
+}
