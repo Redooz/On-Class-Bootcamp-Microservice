a7c92f3c3271fea6ddb1fbe4b4bc498f8f2e8d47
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-12 00:05:37 -0500

    feat: add entity for technologies

---
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/entity/TechnologyEntity.java
@@ -0,0 +1,26 @@
+package com.redoz.onclass.adapters.driven.jpa.mysql.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "technologies")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TechnologyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String direction;
+}
