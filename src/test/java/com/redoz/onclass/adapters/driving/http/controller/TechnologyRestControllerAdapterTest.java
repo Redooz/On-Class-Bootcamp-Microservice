7e8c2d50e8e81e3170531dda300e2d78eee73256
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-13 21:59:57 -0500

    test: add tests for Technology Use Case and Technology Rest Controller; the tests are for findAllTechnologies

--- src/test/java/com/redoz/onclass/adapters/driving/http/controller/TechnologyRestControllerAdapterTest.java
+++ src/test/java/com/redoz/onclass/adapters/driving/http/controller/TechnologyRestControllerAdapterTest.java
@@ -1,6 +1,7 @@
 package com.redoz.onclass.adapters.driving.http.controller;
 
 import com.redoz.onclass.adapters.driving.http.dto.request.CreateTechnologyRequest;
+import com.redoz.onclass.adapters.driving.http.dto.response.FindTechnologyResponse;
 import com.redoz.onclass.adapters.driving.http.mapper.ITechnologyRequestMapper;
 import com.redoz.onclass.domain.api.ITechnologyServicePort;
 import com.redoz.onclass.domain.model.Technology;
@@ -10,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -40,6 +43,75 @@ class TechnologyRestControllerAdapterTest {
 
         verify(technologyServicePort, times(1)).saveTechnology(technology);
         assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnAllTechnologiesWhenExists() {
+        // Given
+        int page = 0;
+        int size = 10;
+        boolean isAsc = true;
+        List<Technology> expectedTechnologies = List.of(
+                new Technology(1L, "Java", "A programming language", "Software Development"),
+                new Technology(2L, "Python", "A programming language", "Data Science")
+        );
+        when(technologyServicePort.findAllTechnologies(page, size, isAsc)).thenReturn(expectedTechnologies);
+        List<FindTechnologyResponse> expectedResponses = expectedTechnologies.stream()
+                .map(technologyRequestMapper::modelToFindResponse)
+                .toList();
+
+        // When
+        ResponseEntity<List<FindTechnologyResponse>> responseEntity = technologyRestControllerAdapter.findAllTechnologies(page, size, isAsc);
+
+        // Then
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedResponses, responseEntity.getBody());
+    }
+
+    @Test
+    void shouldReturnTechnologiesInAscendingOrderWhenIsAscIsTrue() {
+        // Given
+        int page = 0;
+        int size = 10;
+        boolean isAsc = true;
+        List<Technology> expectedTechnologies = List.of(
+                new Technology(1L, "Java", "A programming language", "Software Development"),
+                new Technology(2L, "Python", "A programming language", "Data Science")
+        );
+        when(technologyServicePort.findAllTechnologies(page, size, isAsc)).thenReturn(expectedTechnologies);
+        List<FindTechnologyResponse> expectedResponses = expectedTechnologies.stream()
+                .map(technologyRequestMapper::modelToFindResponse)
+                .toList();
+
+        // When
+        ResponseEntity<List<FindTechnologyResponse>> responseEntity = technologyRestControllerAdapter.findAllTechnologies(page, size, isAsc);
+
+        // Then
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedResponses, responseEntity.getBody());
+    }
+
+    @Test
+    void shouldReturnTechnologiesInDescendingOrderWhenIsAscIsFalse() {
+        // Given
+        int page = 0;
+        int size = 10;
+        boolean isAsc = false;
+        List<Technology> expectedTechnologies = List.of(
+                new Technology(2L, "Python", "A programming language", "Data Science"),
+                new Technology(1L, "Java", "A programming language", "Software Development")
+        );
+        when(technologyServicePort.findAllTechnologies(page, size, isAsc)).thenReturn(expectedTechnologies);
+        List<FindTechnologyResponse> expectedResponses = expectedTechnologies.stream()
+                .map(technologyRequestMapper::modelToFindResponse)
+                .toList();
+
+        // When
+        ResponseEntity<List<FindTechnologyResponse>> responseEntity = technologyRestControllerAdapter.findAllTechnologies(page, size, isAsc);
+
+        // Then
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedResponses, responseEntity.getBody());
     }
 
 }
