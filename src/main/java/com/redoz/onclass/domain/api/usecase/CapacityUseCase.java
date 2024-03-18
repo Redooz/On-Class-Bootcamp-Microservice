246c308e7dc82ab3fefa6fe1ad6630d4fde20044
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-18 00:11:31 -0500

    feat: add service port, use case and exceptions for capacity

---
+++ src/main/java/com/redoz/onclass/domain/api/usecase/CapacityUseCase.java
@@ -0,0 +1,43 @@
+package com.redoz.onclass.domain.api.usecase;
+
+import com.redoz.onclass.domain.api.ICapacityServicePort;
+import com.redoz.onclass.domain.exception.DuplicateTechnologiesException;
+import com.redoz.onclass.domain.exception.ExcessiveTechnologiesException;
+import com.redoz.onclass.domain.exception.InsufficientTechnologiesException;
+import com.redoz.onclass.domain.model.Capacity;
+import com.redoz.onclass.domain.model.Technology;
+import com.redoz.onclass.domain.spi.ICapacityPersistencePort;
+import com.redoz.onclass.domain.util.CapacityConstants;
+
+import java.util.List;
+import java.util.Set;
+
+public class CapacityUseCase implements ICapacityServicePort {
+    private final ICapacityPersistencePort capacityPersistencePort;
+
+    public CapacityUseCase(ICapacityPersistencePort capacityPersistencePort) {
+        this.capacityPersistencePort = capacityPersistencePort;
+    }
+
+    @Override
+    public void saveCapacity(Capacity capacity) {
+        if (capacity.getTechnologies().size() < CapacityConstants.MIN_TECHNOLOGIES) {
+            throw new InsufficientTechnologiesException(capacity.getName());
+        }
+
+        if (capacity.getTechnologies().size() > CapacityConstants.MAX_TECHNOLOGIES) {
+            throw new ExcessiveTechnologiesException(capacity.getName());
+        }
+
+        if (!technologiesAreUnique(capacity.getTechnologies())) {
+            throw new DuplicateTechnologiesException(capacity.getName());
+        }
+
+        capacityPersistencePort.saveCapacity(capacity);
+    }
+
+    private boolean technologiesAreUnique(List<Technology> technologies) {
+        Set<Technology> uniqueTechnologies = Set.copyOf(technologies);
+        return uniqueTechnologies.size() == technologies.size();
+    }
+}
