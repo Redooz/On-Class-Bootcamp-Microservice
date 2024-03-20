6ee8e35d47ffa93d2a536a4306c2aec0d03a01cc
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-20 01:20:12 -0500

    fix: now the capacities are being ignored when mapping to technology from an entity or a dto

--- src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/mapper/ICapacityEntityMapper.java
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/mapper/ICapacityEntityMapper.java
@@ -7,5 +7,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ICapacityEntityMapper {
     CapacityEntity toEntity(Capacity capacity);
+
     Capacity toModel(CapacityEntity capacityEntity);
 }
