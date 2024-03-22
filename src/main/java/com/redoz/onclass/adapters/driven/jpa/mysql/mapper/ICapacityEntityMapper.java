470ab11cb1d413082e168d68a07dab54e3a0ca82
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-22 18:57:17 -0500

    fix: remove unused import

--- src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/mapper/ICapacityEntityMapper.java
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/mapper/ICapacityEntityMapper.java
@@ -3,7 +3,6 @@ package com.redoz.onclass.adapters.driven.jpa.mysql.mapper;
 import com.redoz.onclass.adapters.driven.jpa.mysql.entity.CapacityEntity;
 import com.redoz.onclass.domain.model.Capacity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
