094bd56d3c642f0379129727cc3607661ed06092
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-22 15:11:04 -0500

    fix: change jpa repository entity param from bootcamp to bootcamp entity

--- src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/repository/IBootcampRepository.java
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/repository/IBootcampRepository.java
@@ -1,7 +1,7 @@
 package com.redoz.onclass.adapters.driven.jpa.mysql.repository;
 
-import com.redoz.onclass.domain.model.Bootcamp;
+import com.redoz.onclass.adapters.driven.jpa.mysql.entity.BootcampEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IBootcampRepository extends JpaRepository<Bootcamp, Long> {
+public interface IBootcampRepository extends JpaRepository<BootcampEntity, Long> {
 }
