ca9b5a146b85c12c04be276132c7ffdf76901aa7
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-03-22 15:01:56 -0500

    feat: add interface repository for bootcamp

---
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/repository/IBootcampRepository.java
@@ -0,0 +1,7 @@
+package com.redoz.onclass.adapters.driven.jpa.mysql.repository;
+
+import com.redoz.onclass.domain.model.Bootcamp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IBootcampRepository extends JpaRepository<Bootcamp, Long> {
+}
