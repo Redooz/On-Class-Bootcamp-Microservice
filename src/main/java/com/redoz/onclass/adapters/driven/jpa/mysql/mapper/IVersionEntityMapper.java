ac437edf86918298069cde873b18249fb9215c4f
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-05 22:31:00 -0500

    feat(version): add a mapper for entity

--- src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/mapper/IVersionEntityMapper.java
+++ src/main/java/com/redoz/onclass/adapters/driven/jpa/mysql/mapper/IVersionEntityMapper.java
@@ -4,9 +4,11 @@ import com.redoz.onclass.adapters.driven.jpa.mysql.entity.VersionEntity;
 import com.redoz.onclass.domain.model.Version;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface IVersionEntityMapper {
     VersionEntity toEntity(Version version);
 
-    Version toModel(VersionEntity versionEntity);
+    List<Version> toModelList(List<VersionEntity> versionEntities);
 }
