1e6bd9bb2f7a9ccd6e2a6f08efcef8b06e2639da
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-05 23:53:03 -0500

    feat(version): add dto to create a version

--- src/main/java/com/redoz/onclass/adapters/driving/http/dto/request/CreateVersionRequest.java
+++ src/main/java/com/redoz/onclass/adapters/driving/http/dto/request/CreateVersionRequest.java
@@ -1,14 +1,22 @@
 package com.redoz.onclass.adapters.driving.http.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.redoz.onclass.adapters.driving.http.utils.VersionConstants;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
 public class CreateVersionRequest {
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private final LocalDate startDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private final LocalDate endDate;
+
+    @Min(value = 1, message = VersionConstants.MIN_NUM_OF_STUDENTS)
     private final int numOfStudents;
 }
