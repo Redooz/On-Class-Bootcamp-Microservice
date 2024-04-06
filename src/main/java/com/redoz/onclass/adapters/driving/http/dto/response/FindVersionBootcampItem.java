6034cff73b70bb7e3f6e763a693acc30bb53bc96
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-06 15:45:14 -0500

    feat(version): add a dto for returning a version

---
+++ src/main/java/com/redoz/onclass/adapters/driving/http/dto/response/FindVersionBootcampItem.java
@@ -0,0 +1,15 @@
+package com.redoz.onclass.adapters.driving.http.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FindVersionBootcampItem {
+    private Long id;
+    private String name;
+}
