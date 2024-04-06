1b781cb1c704afe9a8f758c93ab3b006dc39c5d8
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-06 14:55:10 -0500

    feat(version): add controller advisor for version rest controller

---
+++ src/main/java/com/redoz/onclass/configuration/exceptionhandler/VersionControllerAdvisor.java
@@ -0,0 +1,20 @@
+package com.redoz.onclass.configuration.exceptionhandler;
+
+import com.redoz.onclass.domain.exception.InvalidDateException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class VersionControllerAdvisor {
+    @ExceptionHandler(InvalidDateException.class)
+    public ResponseEntity<ExceptionResponse> handleInvalidDateException(InvalidDateException e) {
+        ExceptionResponse response = new ExceptionResponse(e.getMessage(), HttpStatus.BAD_GATEWAY.toString(), LocalDateTime.now());
+        return ResponseEntity.badRequest().body(response);
+    }
+}
